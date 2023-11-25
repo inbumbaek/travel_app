57705e83745cb6e65e334e04e68630b3a0366449
inbumbaek <inbumbaek@gmail.com>
2023-11-25 02:35:32 -0800

    complete navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,23 @@
-import Image from "next/image"
-import Link from "next/link"
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-        <ul className="hidden h-full gap-12 lg:flex">
-          {['']}
-        </ul>
       </Link>
-    </nav>
-  )
-}
 
-export default Navbar
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
