67d7ee6aab04d0eb3ece8642a693bb75f302b850
inbumbaek <inbumbaek@gmail.com>
2023-11-26 16:24:30 -0800

    add button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import Image from "next/image";
+
+type ButtonProps = {
+  type: 'button' | 'submit';
+  title: string;
+  icon?: string;
+  variant: string;
+  full?: boolean;
+}
 
 const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
