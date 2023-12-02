b65a926f4929c2a0e8268c495d572e6ee543a696
inbumbaek <inbumbaek@gmail.com>
2023-12-02 14:39:00 -0800

    add image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,6 +52,18 @@ const Hero = () => {
           />
         </div>
       </div>
+
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+        <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">Location</p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+            <p className="bold-20 text-white">Aguas Calientes</p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
