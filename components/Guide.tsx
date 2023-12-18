b22c0aeff12c8b566adb1cf23871b561bf659df0
inbumbaek <inbumbaek@gmail.com>
2023-12-18 12:07:36 -0800

    add second image

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -23,6 +23,15 @@ const Guide = () => {
           height={580}
           className="w-full object-cover object-center 2xl:rounded-5xl"
         />
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+        <Image 
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+        </div>
       </div>
     </section>
   )
