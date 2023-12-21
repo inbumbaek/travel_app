c309466a58158fc8d3a9adbeea77c0278ec2158d
inbumbaek <inbumbaek@gmail.com>
2023-12-20 17:03:59 -0800

    add h4

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -42,6 +42,7 @@ const Guide = () => {
 
             <div className='flex w-full flex-col'>
               <p className="regular-16 text-gray-20">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
             </div>
           </div>
         </div>
