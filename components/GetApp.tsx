1d05da61ca89aff693b02f59384961b32b17ba78
inbumbaek <inbumbaek@gmail.com>
2023-12-29 15:28:55 -0800

    move on to getapp

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const GetApp = () => {
   return (
-    <div>GetApp</div>
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      <div className="get-app">
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
