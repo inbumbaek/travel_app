e5987da5978596477a951889563b580a94617018
inbumbaek <inbumbaek@gmail.com>
2023-12-31 15:01:07 -0800

    add h2 tag

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -5,7 +5,7 @@ const GetApp = () => {
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>
         </div>
       </div>
     </section>
