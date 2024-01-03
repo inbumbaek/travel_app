b61425a7f4f46f8fa5521901465c8b5794f273c8
inbumbaek <inbumbaek@gmail.com>
2024-01-02 16:45:21 -0800

    add image

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -26,6 +26,9 @@ const GetApp = () => {
             />
           </div>
         </div>
+        <div className="flex flex-1 items-center justify-end">
+          <Image src="/phones.png" alt="phones" width={550} height={870} />
+        </div>
       </div>
     </section>
   )
