151fb7cbd7712420db35e47c49961eb085470e1f
inbumbaek <inbumbaek@gmail.com>
2024-01-06 23:48:15 -0800

    add link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FOOTER_LINKS } from '@/constants'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -23,6 +23,19 @@ const Footer = () => {
                 </ul>
               </FooterColumn>
             ))}
+
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link) => (
+                  <Link
+                    href="/"
+                    key={link.label}
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
 
           </div>
 
