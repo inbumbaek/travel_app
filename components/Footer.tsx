fefa59f59141e689bbff62d6494c3fd7ee4ebccb
inbumbaek <inbumbaek@gmail.com>
2024-01-09 14:32:59 -0800

    add footercolumn

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -72,4 +72,13 @@ type FooterColumnProps = {
   children: React.ReactNode;
 }
 
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  )
+}
+
 export default Footer
