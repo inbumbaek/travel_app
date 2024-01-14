5b4b59390a116a3ed47e05bd942c337e07a89163
inbumbaek <inbumbaek@gmail.com>
2024-01-14 15:36:50 -0800

    change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-22 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+          <Link href={link.href} key={link.key} className="regular-20 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
             {link.label}
           </Link>
         ))}
